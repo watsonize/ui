abe5c628f980d004f966648684718a92080abd2c
Valentin Vogel <18506336+valentin-vogel@users.noreply.github.com>
2025-08-21 02:57:58 +0200

    feat: init ui package

---
+++ packages/ui-components/README.md
@@ -0,0 +1,7 @@
+# @watsonize/ui-components
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test @watsonize/ui-components` to execute the unit tests via [Vitest](https://vitest.dev/).
---
+++ packages/ui-components/src/lib/ui-components.tsx
@@ -0,0 +1,11 @@
+import styles from './ui-components.module.css'
+
+export function WatsonizeUiComponents() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to WatsonizeUiComponents!</h1>
+    </div>
+  )
+}
+
+export default WatsonizeUiComponents
